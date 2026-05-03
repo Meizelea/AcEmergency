1deec83610fe074a45f23f4d22d913e4197ee1d2
Meizelea <shadowkayn12@gmail.com>
2026-05-04 00:58:19 +0800

    Update ProtectedRoute.jsx

--- thesis/src/components/ProtectedRoute.jsx
+++ thesis/src/components/ProtectedRoute.jsx
@@ -1,11 +1,8 @@
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => {
-  // Check localStorage to see if the admin token exists
   const isAuthenticated = localStorage.getItem('isAdminLoggedIn') === 'true';
 
-  // If they are logged in, render the protected pages (represented by <Outlet />)
-  // If not, instantly redirect them to the login page
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
